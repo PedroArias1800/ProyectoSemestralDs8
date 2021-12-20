8af961c709afcb3e48af5ec0fee0453b3e5ea2dc
PedroArias1800 <86125486+PedroArias1800@users.noreply.github.com>
2021-12-20 15:00:17 -0500

    Ya presentamos, este es el último commit

--- ProyectoSemestral/ProyectoSemestral/Form1.vb
+++ ProyectoSemestral/ProyectoSemestral/Form1.vb
@@ -1,4 +1,24 @@
 ﻿Public Class Form1
+
+    'Universidad Tecnológica De Panamá
+    'Facultad De Ingeniería En Sistema Computacionales
+    'Licenciatura En Desarrollo De Software
+    'Proyecto Semestral De Desarrollo De Software VIII
+    '
+    'Integrantes:
+    'Alvarado Jordan / 8-954-1826
+    'Arias Pedro / 8-961-780
+    'Gómez Daniel / E-8-164026
+    'Morales Aurelio / 8-944-1505
+    '
+    'Docente:
+    'José Javier Chirú
+    '
+    'Grupo: 1LS132
+    '
+    'Fecha De Entrega
+    '20/12/2021
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Button1.Left = Me.Width / 2 - (Button1.Width / 2)
         PictureBox1.Left = Me.Width / 2 - (PictureBox1.Width / 2)
